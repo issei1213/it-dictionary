b8c50dad200bd4638779b71d7b8706059196a640
issei1213 <59830008+issei1213@users.noreply.github.com>
2020-02-08 22:22:38 +0900

    jquery修正

--- app/assets/javascripts/index_fover.js
+++ app/assets/javascripts/index_fover.js
@@ -1,9 +1,9 @@
 $(function(){
   $(".line").hover(function() {
-    $(this).stop().animate({'backgroundColor' : "#ffcd00"}, 500);
+    $(this).stop().css({'backgroundColor' : "#ffcd00"}, 500);
   },
   function() {
-    $(this).stop().animate({'backgroundColor' : "#f8f6ef"}, 1000);
+    $(this).stop().css({'backgroundColor' : "#f8f6ef"}, 1000);
   });
 })
 
